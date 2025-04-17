d542e1fc501c06d54e77fd2e6da73612ba241cd7
Kedare Harshvardhan Manoj <harshvardhan2002@caras-zeroday.vlsi.ict.e.titech.ac.jp>
2025-04-17 20:21:41 +0900

    12_pi.cpp

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -4,9 +4,11 @@ int main() {
   int n = 10;
   double dx = 1. / n;
   double pi = 0;
-  for (int i=0; i<n; i++) {
-    double x = (i + 0.5) * dx;
-    pi += 4.0 / (1.0 + x * x) * dx;
-  }
+
+  #pragma omp parallel for
+    for (int i=0; i<n; i++) {
+      double x = (i + 0.5) * dx;
+      pi += 4.0 / (1.0 + x * x) * dx;
+    }
   printf("%17.15f\n",pi);
 }
