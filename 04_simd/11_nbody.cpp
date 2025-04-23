bac0709c7dcad21f8b07b229bd4e7665cb292fef
Harshvardhan1809 <harrygarry2002@gmail.com>
2025-04-24 01:20:06 +0900

    simd/11_nbody.cpp

--- 04_simd/11_nbody.cpp
+++ 04_simd/11_nbody.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <x86intrin.h>
 
 int main() {
   const int N = 8;
@@ -12,15 +13,35 @@ int main() {
     fx[i] = fy[i] = 0;
   }
   for(int i=0; i<N; i++) {
-    for(int j=0; j<N; j++) {
-      if(i != j) {
-        float rx = x[i] - x[j];
-        float ry = y[i] - y[j];
-        float r = std::sqrt(rx * rx + ry * ry);
-        fx[i] -= rx * m[j] / (r * r * r);
-        fy[i] -= ry * m[j] / (r * r * r);
-      }
-    }
+
+    // 座標のベクトルを作成する
+    __m512 x_vec = _mm512_load_ps(&x);
+    __m512 y_vec = _mm512_load_ps(&y);
+
+    // 実質的にjが表している要素のベクトル
+    __m512 x_plus = _mm512_set1_ps(x[i]);
+    __m512 y_plus = _mm512_set1_ps(y[i]);
+    __m512 m_vec = _mm512_set1_ps(m[i]);
+
+    // マスクベクトルの生成
+    float mask_arr[N] = {1};
+    mask_arr[i] = 0;
+    __m512 mask = _mm512_load_ps(&mask_arr);
+
+    __m512 rxvec = _mm512_sub_ps(x_vec, _mm512_mul_ps(x_plus, mask));
+    __m512 ryvec = _mm512_sub_ps(y_vec, _mm512_mul_ps(y_plus, mask));
+
+    __m512 r2vec = _mm512_add_ps(_mm512_mul_ps(rxvec, rxvec), _mm512_mul_ps(ryvec, ryvec));
+    __m512 r3vec = _mm512_mul_ps(_mm512_rsqrt14_ps(r2vec), r2vec);
+
+    // 力の計算
+    __m512 fxvec = _mm512_div_ps(_mm512_mul_ps(rxvec, m_vec), r3vec);
+    __m512 fyvec = _mm512_div_ps(_mm512_mul_ps(ryvec, m_vec), r3vec);
+
+    // 計算結果をreduceして元の配列に保存する
+    fx[i] = _mm512_reduce_add_ps(fxvec);
+    fy[i] = _mm512_reduce_add_ps(fyvec);
+
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
 }
